b2057807c75d71019e41c6e461cacf7c0d902105
Schierz, Robert <s65727@beuth-hochschule.de>
2017-01-09 03:43:17 +0100

    PDF anzeige funktioniert

--- code/js/controller.js
+++ code/js/controller.js
@@ -6,61 +6,74 @@
 var app = angular.module('app', []);
 
 var initTimer = 0;
+var filter = {};
 
-var filter = {
-    schoolID: null,
-    courseID: null
-};
+function resetFilter() {
+    filter = {
+        schoolID: null,
+        courseID: null
+    };
+}
+
+resetFilter();
 
 
-
-
-var contentAppController = app.controller('contentcontroller', function($scope, $rootScope, $http, sharedScopeofContentData){
+var contentAppController = app.controller('contentcontroller', function ($scope, $rootScope, $http, sharedScopeofContentData, sharedScopeofFilterData) {
 
     sharedScopeofContentData.addList($scope);
 
-    $scope.initContent = function(){
+    $scope.initContent = function () {
 
 
-            if(initTimer == 0){
-                $http.post('../php/getClauses.php').then(function(response){
+        if (initTimer == 0) {
+            $http.post('../php/getClauses.php').then(function (response) {
 
-                    $rootScope.clauses = response.data;
+                $rootScope.clauses = response.data;
 
 
-                });
-                initTimer += 1;
-            }
+            });
+            initTimer += 1;
+        }
 
 
     };
+
+    $scope.getPdfUrl = function(){
+        var contentscope = sharedScopeofFilterData.getList();
+
+        return "http://www.klausurenhub.bplaced.net/" + contentscope.Path;
+
+    }
+
+
 })
 
 
-
-var filterAppController = app.controller('filtercontroller',  function($scope, $rootScope, $http, sharedScopeofContentData ) {
-
+var filterAppController = app.controller('filtercontroller', function ($scope, $rootScope, $http, sharedScopeofContentData, sharedScopeofFilterData) {
 
 
-    $scope.schoolheader = "Hochschulen";
-    $scope.courseheader = "Studiengang";
+    $scope.setFilterStandardValues = function () {
+        $scope.schoolheader = "Hochschulen";
+        $scope.courseheader = "Studiengang";
+    }
+
 
     filter;
 
-    $scope.loadAvailableOptions = function(scopeVariableName, database){
+    $scope.loadAvailableOptions = function (scopeVariableName, database) {
 
-        var requestData = {'database' : database};
+        var requestData = {'database': database};
 
-       $http.post('../php/getAvailableOptions.php', requestData)
-           .then(function(response){
-               $scope[scopeVariableName] = response.data;
+        $http.post('../php/getAvailableOptions.php', requestData)
+            .then(function (response) {
+                $scope[scopeVariableName] = response.data;
 
 
-           })
+            })
 
     };
 
-    $scope.setFilterelementToDecision = function(selectedItem, header, source){
+    $scope.setFilterelementToDecision = function (selectedItem, header, source) {
         $scope[header] = selectedItem[source + "Name"];
         filter[source + "ID"] = selectedItem[source + "ID"]
 
@@ -68,14 +81,12 @@ var filterAppController = app.controller('filtercontroller',  function($scope, $
         $scope.updateContent();
 
 
-
     };
 
-    $scope.updateContent = function(){
+    $scope.updateContent = function () {
 
 
-
-        var filterQuery = "SELECT clauseID , clauseName, Path, Uploader, courseName, schoolName, clauses.schoolID, clauses.courseID "+
+        var filterQuery = "SELECT clauseID , clauseName, Path, Uploader, courseName, schoolName, clauses.schoolID, clauses.courseID " +
             "FROM clauses " +
             "LEFT JOIN courses " +
             "ON clauses.courseID = courses.courseID " +
@@ -83,19 +94,19 @@ var filterAppController = app.controller('filtercontroller',  function($scope, $
             "ON clauses.schoolID = schools.schoolID " +
             "WHERE ";
 
-        for(var key in filter){
-            if(filter.hasOwnProperty(key) && filter[key] != null){
-                filterQuery += ("clauses." + key +" = " + "'"+filter[key]+"' AND ");
+        for (var key in filter) {
+            if (filter.hasOwnProperty(key) && filter[key] != null) {
+                filterQuery += ("clauses." + key + " = " + "'" + filter[key] + "' AND ");
 
             }
         }
 
-        filterQuery = filterQuery.slice(0, - 4);
+        filterQuery = filterQuery.slice(0, -4);
 
-        var requestData = {'query' : filterQuery };
+        var requestData = {'query': filterQuery };
 
         $http.post('../php/getFilteredQuery.php', requestData)
-            .then(function(response){
+            .then(function (response) {
 
                 $rootScope.clauses = response.data;
 
@@ -103,28 +114,45 @@ var filterAppController = app.controller('filtercontroller',  function($scope, $
             })
     }
 
-    $scope.resetFilter = function(){
+    $scope.resetFilter = function () {
         initTimer = 0;
         var sharedscope = sharedScopeofContentData.getList();
 
-        sharedscope.url =  "../loadedhtml/content/clauseOverviewDisplay.html";
+        sharedscope.url = "../loadedhtml/content/clauseOverviewDisplay.html";
         sharedscope.initContent();
-
+        $scope.setFilterStandardValues();
+        resetFilter();
     }
-
 
 
 });
 
 
-app.service('sharedScopeofContentData', function() {
+app.service('sharedScopeofContentData', function () {
     var myList = {};
 
-    var addList = function(newObj) {
-        myList= newObj;
+    var addList = function (newObj) {
+        myList = newObj;
     }
 
-    var getList = function(){
+    var getList = function () {
+        return myList;
+    }
+
+    return {
+        addList: addList,
+        getList: getList
+    };
+});
+
+app.service('sharedScopeofFilterData', function () {
+    var myList = {};
+
+    var addList = function (newObj) {
+        myList = newObj;
+    }
+
+    var getList = function () {
         return myList;
     }
 
@@ -135,34 +163,30 @@ app.service('sharedScopeofContentData', function() {
 });
 
 
-
-app.directive('filterelements', function() {
+app.directive('filterelements', function () {
     return {
-        templateUrl:  "../loadedhtml/mainpage/filter.html"
+        templateUrl: "../loadedhtml/mainpage/filter.html"
     };
 });
 
 
-app.directive('displaycontentoverview', function() {
+app.directive('displaycontentoverview', function (sharedScopeofContentData, sharedScopeofFilterData) {
 
-    function handleContent($scope, elem, attrs){
+    function handleContent($scope, elem, attrs) {
 
         $scope.url = "../loadedhtml/content/clauseOverviewDisplay.html";
 
 
-
-        $scope.back = function(){
+        $scope.back = function () {
 
             $scope.url = "../loadedhtml/content/clauseOverviewDisplay.html";
 
         }
 
-        $scope.changeToContentView = function(clause){
-
+        $scope.changeToContentView = function (clause) {
+            sharedScopeofFilterData.addList(clause);
             $scope.clickedClause = clause;
             $scope.url = "../loadedhtml/content/clauseContentDisplay.html";
-
-
 
         }
     }
@@ -170,9 +194,6 @@ app.directive('displaycontentoverview', function() {
     return {
         template: '<div ng-include="url"></div>',
         link: handleContent
-
-
-
 
     };
 });
